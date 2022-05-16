792b5f30bd9fa5ffa9f5659772d2f08f7b627988
gianlucarloni <gianlucarloni8@gmail.com>
2022-05-16 09:52:12 +0200

    modified params for runs

--- settings.py
+++ settings.py
@@ -6,8 +6,8 @@ base_architecture = 'resnet18' #'resnet50'
 
 img_size = 224 #124# 224 #336
 num_classes = 2 
-num_prots_per_class = 10
-num_filters = 512 #256 #128 #256
+num_prots_per_class = 40 #40 #TODO
+num_filters = 512 #256 #128 #256 #TODO
 prototype_shape = (num_classes*num_prots_per_class, num_filters, 1, 1) #40 #60 #16 #40
 prototype_activation_function = 'log'
 # add_on_layers_type = 'regular'
@@ -15,7 +15,7 @@ add_on_layers_type = 'bottleneck'
 
 wd = 1e-3 #TODO
 num_layers_to_train = 20 #TODO aggiunto da noi
-dropout_proportion = 0.4 #TODO aggiunto noi
+dropout_proportion = 0.4 #0.7 #TODO aggiunto noi
 
 data_path = './datasets/' #
 train_dir = data_path + 'push_augmented/' #
@@ -31,15 +31,15 @@ train_batch_size = 40 #40
 test_batch_size = 2
 train_push_batch_size = 40 #90 #40 #4
 
-joint_optimizer_lrs = {'features': 1e-07, #1e-06,#1e-4 #TODO
-                       'add_on_layers': 1e-07, #3e-3,
-                       'prototype_vectors': 1e-07} #3e-3}
+joint_optimizer_lrs = {'features': 1e-06, #1e-06,#1e-4 #TODO
+                       'add_on_layers': 1e-06, #3e-3,
+                       'prototype_vectors': 1e-06} #3e-3}
 joint_lr_step_size = 10 #5 #TODO
 
-warm_optimizer_lrs = {'add_on_layers': 1e-07, #3e-3,
-                      'prototype_vectors': 1e-07} #3e-3}
+warm_optimizer_lrs = {'add_on_layers': 1e-06, #3e-3,
+                      'prototype_vectors': 1e-06} #3e-3}
 
-last_layer_optimizer_lr = 1e-07 #1e-4
+last_layer_optimizer_lr = 1e-06 #1e-4
 
 coefs = {
     'crs_ent': 1,
@@ -48,7 +48,7 @@ coefs = {
     'l1': 1e-4
 }
 
-num_train_epochs = 1000 #TODO
+num_train_epochs = 1000 #TODO era 1000 con EarlyStopping
 num_warm_epochs = 5
 
 push_start = 10
