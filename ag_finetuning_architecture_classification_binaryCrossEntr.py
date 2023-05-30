9af9720fa814cdfeb5dc323fca8ede6222ca35b4
Andrea Berti <andrea.berti11235@icloud.com>
2023-05-30 12:56:13 +0200

    Use patience instead of joint_lr_step_size in ReduceLROnPlateau

--- ag_finetuning_architecture_classification_binaryCrossEntr.py
+++ ag_finetuning_architecture_classification_binaryCrossEntr.py
@@ -832,7 +832,7 @@ for model_name in model_names:
         if scheduler_name == 'StepLR':
             joint_lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer_ft, step_size=joint_lr_step_size, gamma=gamma_value, verbose=True)
         elif scheduler_name == 'ReduceLROnPlateau':
-            joint_lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer_ft, mode='min', factor=factor, patience=joint_lr_step_size, verbose=True)
+            joint_lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer_ft, mode='min', factor=factor, patience=patience, verbose=True)
             # joint_lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer_ft, mode='max', factor=factor, patience=joint_lr_step_size, verbose=True)
             # joint_lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer_ft, mode='max', factor=factor, patience=patience, threshold=threshold, verbose=True)
 
