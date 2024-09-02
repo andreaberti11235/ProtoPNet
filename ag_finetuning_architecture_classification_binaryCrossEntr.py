c8b6e93babcbccac096ef1b26e23e20a50474c3c
Andrea BERTI AB279692 <andrea.berti11235@icloud.com>
2024-09-02 13:51:49 +0200

    Reduced step of LR scheduler to 5

--- ag_finetuning_architecture_classification_binaryCrossEntr.py
+++ ag_finetuning_architecture_classification_binaryCrossEntr.py
@@ -72,7 +72,7 @@ model_names = [actual_model_name+f'_finetuning_last_{num_layers_to_train}_layers
 
 seed = args.seed
 
-joint_lr_step_size = 50 #5
+joint_lr_step_size = 5 #5
 gamma_value = 0.3
 factor = 0.5
 threshold = 1e-2
